0820f7ec764b9ffa65d3528de4e4d02700127b5c
chirag853 <chirag@adomantra.com>
2026-08-29 09:31:39 +0530

    commmited by chirag

--- resources/views_backup_13_august_2026/customer/create-shipment.blade.php
+++ resources/views_backup_13_august_2026/customer/create-shipment.blade.php
@@ -6,7 +6,7 @@
     <!-- Meta Tags -->
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Contacts | CRMS - Advanced Bootstrap 5 Admin Template for Customer Management</title>
+    <title>Customer | Dashboard</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
    
