9adef50347c7efa2da24abed5917507c559a0e7a
chirag853 <chirag@adomantra.com>
2026-08-29 19:51:58 +0530

    commited by chirag

--- resources/views_backup_13_august_2026/website_include/header.blade.php
+++ resources/views_backup_13_august_2026/website_include/header.blade.php
@@ -22,7 +22,7 @@
         <meta name="keywords" content="courier, shipping, logistics, international shipping, express freight, ecommerce logistics, warehousing">
     @endif
     <!-- Favicon icons -->
-    <link rel="icon" href="{{ asset('public/website_images/fav-icon.png') }}" type="image/x-icon">
+    <link rel="icon" href="{{ asset('website_images/fav-icon.png') }}" type="image/x-icon">
     <!-- Bootstrap 5 CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Custom CSS -->
